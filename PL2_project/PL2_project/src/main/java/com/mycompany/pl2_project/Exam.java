d936eb773149a4ba86e9f6487da9e70a9e5bffcb
Mahmoud Mohamed Elnahrawy <119618860+NA7RAWY@users.noreply.github.com>
2024-09-23 22:59:08 +0300

    Delete PL2_project/PL2_project directory

--- PL2_project/PL2_project/src/main/java/com/mycompany/pl2_project/Exam.java
+++
@@ -1,8 +0,0 @@
-package com.mycompany.pl2_project;
-public class Exam {
-    private String sub_name;
-    private String sub_code;
-    private String question;
-    private String answer;
-    
-}
